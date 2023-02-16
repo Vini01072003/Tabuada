af7903f1e27ae0cd6ffa811d7681fb8529e9887d
VINICIUS ALVES DOS SANTOS <22282103@CFP127ALUNO.LOCAL>
2023-02-16 09:28:37 -0300

    trabalho finalizado

---
+++ modulo/tabuada.js
@@ -0,0 +1,28 @@
+const tabuada = function(multiplicandoInicio, multiplicandoFim, contadorInicio, contadorFim) {
+
+    let resultado
+    let ValorDoMultiplicando = multiplicandoInicio
+    let multiplicandoFinal = multiplicandoFim
+    let contadorInicial
+    let contadorFinal = contadorFim
+
+
+    while (ValorDoMultiplicando <= multiplicandoFinal) {
+        console.log(`\n*********** Tabuada do ${ValorDoMultiplicando} *********** \n`)
+
+        contadorInicial = contadorInicio
+        while (contadorInicial <= contadorFinal) {
+            resultado = ValorDoMultiplicando * contadorInicial
+
+            console.log(`${ValorDoMultiplicando} x ${contadorInicial} = ${resultado}`)
+            contadorInicial++
+        }
+        ValorDoMultiplicando++
+
+    }
+
+
+}
+
+
+module.exports = { tabuada }
